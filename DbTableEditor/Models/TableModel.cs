8d66127eac168587ab47b727f8ef9db2d7cb9826
Андрей Полчихин <Polchihin@yandex.ru>
2025-10-10 16:48:04 +0500

    Минорные изменения

---
+++ DbTableEditor/Models/TableModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DbTableEditor.Models
+{
+    public class TableModel
+    {
+        public int IdTable { get; set; }
+        public string NameTable { get; set; } = string.Empty;
+    }
+}
--- DbTableEditor/ViewModels/MainViewModel.cs
+++ DbTableEditor/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using SalesAnalysis.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesAnalysis.Data;
 
 namespace DbTableEditor.ViewModels
 {
@@ -6,14 +7,46 @@ namespace DbTableEditor.ViewModels
     {
         private readonly MyDbContext _context;
 
+        public MainViewModel()
+        {
+
+        }
+
         public MainViewModel(MyDbContext context)
         {
             _context = context;
+
+            if(CountModels() == 1)
+            {
+                CountModels();
+
+                LoadData();
+            }
         }
 
         public int CountModels()
         {
+            if (_context == null)
+            {
+                return 0;
+            }
+
             return _context.Database.CanConnect() ? 1 : 0;
+        }
+
+        public void LoadData()
+        {
+            //var tables = _context.Set<Models.TableModel>().ToList();
+
+                var tableNames = _context.Model
+                    .GetEntityTypes()
+                    .Select(t => t.GetTableName())
+                    .Distinct()
+                    .ToList();
+
+                foreach (var name in tableNames)
+                    Console.WriteLine(name);
+            
         }
     }
 }
